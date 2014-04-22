2d6c4e9c120dd890c8d2fde625704bbb7d2d81d8
Bruno Diniz <ferreiradiniz@msn.com>
2014-04-22 18:11:30 -0300

    Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teleneo::Application.config.secret_key_base = 'ebdaf4388e254d7d3c24f8e771eff3140868332e8d1c03493e88565064f188a9fff34ba42afff7627c235ef184c97a5fd2f6cf91a3d04da6491c2ed5e714139d'
